39c8505aa4b4527beeb7e3a6e832f4af312b00fb
nulltitleexception <clbprfftt@gmail.com>
2015-05-24 14:06:03 -0400

    ip fix

--- newuser.js
+++ newuser.js
@@ -18,7 +18,7 @@ JS_GAME.game = (function () {
 			document.getElementById("loginInfo").pass1.value = "PASSWORDS DO NOT MATCH";
 			return;
 		}
-		socket = new WebSocket("ws:25.117.117.69:38734");
+		socket = new WebSocket("ws:167.88.120.57:38734");
 
 		socket.onopen = function() {
 			connected = true;
