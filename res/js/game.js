b2a454c4e3fa0ca8d87b920115b004b75dfea036
nulltitleexception <clbprfftt@gmail.com>
2015-06-14 00:12:41 -0400

    comment

--- res/js/game.js
+++ res/js/game.js
@@ -171,6 +171,7 @@ JS_GAME.game = (function () {
   	ySize = typeof ySize !== 'undefined' ? ySize : spriteHeight;
   	sheetCols = Math.floor(getImage(name).width / spriteWidth);
   	sheetRows = Math.floor(getImage(name).height / spriteHeight);
+  	//console.log all params of following call?
   	context.drawImage(getImage(name), (id % sheetCols) * spriteWidth, Math.floor(id / sheetRows) * spriteHeight, spriteWidth, spriteHeight, x, y, xSize, ySize);
   }
 
