4e4cdf673354bc9aadbed7034fb0016e574a2dd4
nulltitleexception <clbprfftt@gmail.com>
2015-06-24 23:52:48 -0400

    logging

--- res/js/game.js
+++ res/js/game.js
@@ -156,7 +156,7 @@ JS_GAME.game = (function() {
             mx = Math.floor(mousePos.x + 0.49);
             my = Math.floor(mousePos.y + 0.49);
             mangle = Math.atan((my - (windowHeight / 2)) / (mx - (windowWidth / 2))) + ((mx >= (windowWidth / 2)) ? (Math.PI / 2) : (Math.PI / -2));
-            sangle = mangle - (Math.PI / 2);
+            sangle = (Math.PI / 2) - mangle;
             if (sangle < 0) {
                 sangle += (Math.PI * 2);
             }
