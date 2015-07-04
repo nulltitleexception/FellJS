7a24a66e88ddf651d243137fd002ce43459fc285
nulltitleexception <clbprfftt@gmail.com>
2015-07-04 16:56:17 -0400

    more drawing + no mipmap

--- res/js/graphics.js
+++ res/js/graphics.js
@@ -107,7 +107,7 @@ GRAPHICS.renderer = function(canv) {
         gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_NEAREST);
-        gl.generateMipmap(gl.TEXTURE_2D);
+        //gl.generateMipmap(gl.TEXTURE_2D);
         gl.bindTexture(gl.TEXTURE_2D, null);
     }
 
@@ -126,6 +126,7 @@ GRAPHICS.renderer = function(canv) {
         gl.uniform2f(shader.halfScreenUniform, canvas.width / 2, canvas.height / 2);
 
         gl.drawArrays(gl.TRIANGLE_STRIP, 0, buffer.numItems);
+        setTimeout(drawScene, 10);
     }
 
 
