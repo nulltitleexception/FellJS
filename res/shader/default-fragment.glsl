f84bed316ff2e9eadf9341af70d3ed3a768c22e3
nulltitleexception <clbprfftt@gmail.com>
2015-07-04 17:20:02 -0400

    shader testing

--- res/shader/default-fragment.glsl
+++ res/shader/default-fragment.glsl
@@ -6,6 +6,5 @@ uniform sampler2D texSampler;
 
 void main(void) {
 	gl_FragColor = texture2D(texSampler, vec2(texCoord.s, texCoord.t));
-	gl_FragColor.r = 1.0;
-	gl_FragColor.a = 1.0;
+	gl_FragColor = vec4(1.0,1.0,1.0,1.0);
 }
