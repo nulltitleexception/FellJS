b8f288d91670460e04cdd57efbd70e58fa76c052
nulltitleexception <clbprfftt@gmail.com>
2015-07-04 17:23:50 -0400

    tex params

--- res/js/graphics.js
+++ res/js/graphics.js
@@ -108,7 +108,7 @@ GRAPHICS.renderer = function(canv) {
         gl.bindTexture(gl.TEXTURE_2D, textureObj.texture);
         gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.CLAMP_TO_EDGE);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
         //gl.generateMipmap(gl.TEXTURE_2D);
         gl.bindTexture(gl.TEXTURE_2D, null);
         textureObj.ready = true;
--- res/shader/default-fragment.glsl
+++ res/shader/default-fragment.glsl
@@ -6,5 +6,4 @@ uniform sampler2D texSampler;
 
 void main(void) {
 	gl_FragColor = texture2D(texSampler, vec2(texCoord.s, texCoord.t));
-	gl_FragColor = vec4(1.0,1.0,1.0,1.0);
 }
