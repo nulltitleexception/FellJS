8f3deffc287e758e807a3972874823de5ece2ec9
nulltitleexception <clbprfftt@gmail.com>
2015-07-04 16:58:44 -0400

    active texture 0

--- res/js/graphics.js
+++ res/js/graphics.js
@@ -103,6 +103,7 @@ GRAPHICS.renderer = function(canv) {
     }
 
     function handleTextureLoaded(image, texture) {
+        gl.activeTexture(gl.TEXTURE0);
         gl.bindTexture(gl.TEXTURE_2D, texture);
         gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
