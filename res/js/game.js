6aaa9c08b1a730fe1f558f6cb2732ee9cdc514e9
nulltitleexception <clbprfftt@gmail.com>
2015-07-18 16:56:50 -0400

    only drawing necessary tiles

--- res/js/game.js
+++ res/js/game.js
@@ -187,10 +187,8 @@ JS_GAME.game = (function() {
             //context.fillText(nameText, (gPIVX(e.x) - (context.measureText(nameText).width / 2)) + (e.width / 2), gPIVY(e.y) - entityNameOffsetY);
         }
         //draw Tiles
-        console.log(Math.floor(Math.max(((playerData.x - (canvas.width/2))/32),0)));
-        console.log(Math.floor(Math.max(((playerData.y - (canvas.height/2))/32),0)));
-        for (a = Math.floor(Math.max(((playerData.x - (canvas.width/2))/32),0)); a < tilesWidth; a++) {
-            for (b = Math.floor(Math.max(((playerData.y - (canvas.height/2))/32),0)); b < tilesHeight; b++) {
+        for (a = Math.floor(Math.max(((playerData.x - (canvas.width/2))/32),0)); a < Math.max(Math.floor(((playerData.x + (canvas.width/2))/32))+1, tilesWidth); a++) {
+            for (b = Math.floor(Math.max(((playerData.y - (canvas.height/2))/32),0)); b < Math.max(Math.floor(((playerData.y + (canvas.height/2))/32))+1, tilesHeight); b++) {
                 if (tiles[a][b].id >= 0) {
                     //TODO
                     //drawImageSection("tilesheet", gPIVX(a * singleTileWidth), gPIVY(b * singleTileWidth), tiles[a][b].id, singleTileWidth, singleTileWidth);
