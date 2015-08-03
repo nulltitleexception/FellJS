4a93edd9177593b3008fb21ddcf25aaa524a68fd
nulltitleexception <clbprfftt@gmail.com>
2015-08-03 19:22:09 -0400

    semicolon

--- res/js/graphics.js
+++ res/js/graphics.js
@@ -82,7 +82,7 @@ GRAPHICS.renderer = (function(canv) {
         var buf = gl.createBuffer();
         buf.bind = function() {
             gl.bindBuffer(gl.ARRAY_BUFFER, this);
-        }
+        };
         gl.bindBuffer(gl.ARRAY_BUFFER, buf);
         var vertices = [
             width / 2.0, height / 2.0,
